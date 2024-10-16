e3e3d8d840164aa1f033c5e515ab7bd1d147e76c
ismailalpha1 <ijalali626@gmail.com>
2024-10-16 10:42:45 +0200

    first commit

---
+++ 1st.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+
+int main(){
+    char ch;
+    char pass[20];
+
+    ch = getch();
+
+    if(ch==13)
+    {
+        pass[i]='\0';
+        break;
+    }else{
+        pass[i++] = ch;
+        printf["*"];
+    }
+
+    printf("You entered : %s\n ", pass);
+
+    FILE * ptr;
+    ptr = fopen("password.txt","r")
+    fscanf(ptr, "%s",saved_pass);
+
+    fclose(ptr);
+    printf("Saved password %s\n", saved_pass);
+    int l = strcmp[pass,saved_pass]; //to compare two strings
+
+    if(l==0)
+    {
+        printf("LOGIN SUCCESSFULLY\n");
+    }else{
+        printf("INCORRECT PASSWORD\n");
+    }
+    return 0;
+}
+
